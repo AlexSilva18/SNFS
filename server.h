26b56d9d319f11bef8b74b21c983fd781b2d4561
AlexSilva18 <alex.rsilva18@gmail.com>
2018-11-25 19:06:38 -0500

    headers

--- server.h
+++ server.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <netdb.h>
+//#include "threads.h"
 
 #endif
